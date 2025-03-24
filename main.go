152592f6fab87b02c42f4fd032288e629534fd77
mabittar <ma_bittar@yahoo.com.br>
2025-03-24 18:28:28 -0300

    add new ping route

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,10 +11,19 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, Golang API!")
 }
 
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+	response := map[string]string{"message": "pong"}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK) // Explicitly setting 200 OK
+	json.NewEncoder(w).Encode(response)
+}
+
 func main() {
-	http.HandleFunc("/", helloHandler)
+	mux := http.NewServeMux() // Create a new ServeMux instance
+	mux.HandleFunc("/", helloHandler)
+	mux.HandleFunc("/ping", pingHandler)
 
 	port := "8080"
 	fmt.Println("Server running on port " + port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, mux)) // Use the mux here
 }
